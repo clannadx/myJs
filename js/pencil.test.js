94b6888f8e6e556d72999b4db61b17015f3fcaec
clannadx <xxi124@sina.com>
2018-08-28 22:21:04 +0800

    'jest-start'

--- js/pencil.test.js
+++ js/pencil.test.js
@@ -1,7 +1,3 @@
-// 污染.持久化
-function assert (expression, message) {
-  console.log(expression, message)
-}
 
 // 用例 test - case
 // assert(query('hello', '?hello=test') === 'test', 'test hello = test')
@@ -9,28 +5,12 @@ function assert (expression, message) {
 // assert(query('hello', '?hello=test&hello2=3') === 'test', 'test hello = test with multiple param')
 // console.log(query('hello', '?hello=test'))
 
-function testCase (message, tests) {
-  var total = 0
-  var success = 0
-  console.log(message + ":");
-  
-  for (var test in tests) {
-    total += 1
-    var ret = tests[test](test)
-    if (ret) {
-      success += 1
-    }
-  }
-  console.log('success/total  ' + success + '/' + total)
-}
 
-// 零散
-// 测试用例   断言一般包含在用例里面
-testCase('Query', {
-  'test hello = test': (message) => {
-    assert(query('hello', '?hello=test') === 'test', message)
-  },
-  'test hello = empty': (message) => {
-    assert(query('hello', '?hello=') === '' ,message)
-  }
+
+
+// testCase  | assert 
+const Pencil = require('./pencil');
+test('query',() => {
+  //assert
+  expect(Pencil.query('hello','?hello=test')).toBe('test')
 })
